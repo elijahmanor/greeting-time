31bbc9bb70b3f2da887342eaa56d111e47ef29e4
TheGreyDiamond <s-einkauf@oest-rs.de>
2021-10-11 19:07:53 +0200

    Added optinal flag

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-module.exports = function(date) {
+module.exports = function(date, includeGoodNight=false) {
   const hour = date.getHours();
   if (hour >= 5 && hour < 12) {
     return "Good morning";
   } else if (hour >= 12 && hour < 17) {
     return "Good afternoon";
-  } else if ((hour >= 17 && hour <= 23) || hour < 5) {
+  } else if ((hour >= 17 && hour <= 23) || hour < 5 && includeGoodNight == false) {
     return "Good evening";
+  } else if ((hour >= 17 && hour <= 20)) {
+    return "Good evening";
+  } else if ((hour >= 21 && hour <= 23) || hour < 5 && includeGoodNight) {
+    return "Good night";
   }
 };
+
